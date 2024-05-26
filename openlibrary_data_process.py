551c6f064645d0977f5a6c7c687ce5c3922eb079
baberabb <92168766+baberabb@users.noreply.github.com>
2024-05-26 13:28:05 +0500

    process files to jsnol

--- openlibrary_data_process.py
+++ openlibrary_data_process.py
@@ -1,10 +1,15 @@
-""" This script processes the bulk download data from the Open Library project."""
+"""This script processes the bulk download data from the Open Library project."""
 
 import csv
 import ctypes as ct
+import pandas as pd
 import os
-
-INPUT_PATH = "./data/unprocessed/"
+from tqdm import tqdm
+import json
+import polars as pl
+from itertools import batched
+# the folder where the filesforprocessing files are downloaded and extracted to.
+INPUT_PATH = "/Users/baber/Downloads/ol_dumps"
 OUTPUT_PATH = "./data/processed/"
 
 filesforprocessing = [
@@ -13,18 +18,63 @@ filesforprocessing = [
     "ol_dump_works.txt",
 ]
 
+
+if __name__ == '__main__':
+
 # See https://stackoverflow.com/a/54517228 for more info on this
-csv.field_size_limit(int(ct.c_ulong(-1).value // 2))
+    csv.field_size_limit(int(ct.c_ulong(-1).value // 2))
 
-for file in filesforprocessing:
-    with open(os.path.join(OUTPUT_PATH, file), "w", newline="", encoding="utf-8") as csv_out:
-        csvwriter = csv.writer(
-            csv_out, delimiter="\t", quotechar="|", quoting=csv.QUOTE_MINIMAL
-        )
+    for file in filesforprocessing:
+        with open(os.path.join(OUTPUT_PATH, f"{file}.jsonl"), "w", newline="", encoding="utf-8") as csv_out:
+            # csvwriter = csv.writer(
+            #     csv_out, delimiter="\t", quotechar="|", quoting=csv.QUOTE_MINIMAL
+            # )
 
-        with open(os.path.join(INPUT_PATH, file), "r", encoding="utf-8") as csv_in:
-            csvreader = csv.reader(csv_in, delimiter="\t")
-            for row in csvreader:
-                if len(row) > 4:
-                    csvwriter.writerow(
-                        [row[0], row[1], row[2], row[3], row[4]])
+            with open(os.path.join(INPUT_PATH, file), "r", encoding="utf-8") as csv_in:
+                count = 0
+                csvreader = csv.reader(csv_in, delimiter="\t")
+                for row in tqdm(csvreader, unit_scale=True, unit="rows", desc=f"Processing {file}"):
+                    # if count == 1000:
+                    #     break
+                    if len(row) > 4:
+                        count += 1
+                        row: dict = json.loads(row[4])
+                        if file == "ol_dump_editions.txt":
+                            row.pop("covers", None)
+                            row.pop("physical_format", None)
+                            row.pop("type", None)
+                            row.pop("created", None)
+                            row.pop("last_modified", None)
+                            row.pop("weight", None)
+                            row.pop("physical_dimensions", None)
+                            row.pop("latest_revision", None)
+                            row.pop("revision", None)
+                            work = [x.get("key") for x in row.get("works", [{}])]
+                            work = [x for x in work if x]
+                            row["works"] = work
+                        elif file == "ol_dump_authors.txt":
+                            row.pop("created", None)
+                            row.pop("type", None)
+                            row.pop("last_modified", None)
+                            row.pop("revision", None)
+                            row.pop("latest_revision", None)
+                        elif file == "ol_dump_works.txt":
+                            row.pop("created", None)
+                            row.pop("covers", None)
+                            row.pop("last_modified", None)
+                            row.pop("latest_revision", None)
+                            row.pop("revision", None)
+                            row.pop("type", None)
+                            try:
+                                author = [x.get("author", {}).get("key") for x in row.get("authors", [{}])]
+                                author = [x for x in author if x]
+                                row["authors"] = author
+                            except:
+                                author = [x.get("author") for x in row.get("authors", [{}])]
+                                author = [x for x in author if x]
+                                row["authors"] = author
+
+                        json.dump(
+                            row, csv_out, sort_keys=True)
+                        csv_out.write("\n")
+
